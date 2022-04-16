3298813cfff939cb3ed1a8035377ff7b8b7fcf3f
Nnder <asfahaikrkt6k56@yandex.ru>
2022-04-16 22:27:36 +0500

    axios

---
+++ axios/index.js
@@ -0,0 +1,19 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+const url = 'https://opoznai.bg/all/sort:popular_ever';
+
+async function getLinks(url){
+    const links = [];
+    const response = await axios.get(url);
+    const $ = cheerio.load(response.data);
+
+    $('.article_image').each(function(){
+        links.push($(this).attr('href'));
+    });
+
+    return links
+}
+
+getLinks(url).then(data=> console.log(data))
+
--- puppeteer/main.js
+++ puppeteer/main.js
@@ -3,9 +3,9 @@ const puppeteer = require('puppeteer');
 (async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto('https://opoznai.bg/all/sort:popular_ever');
-    //await page.waitForRequest()
+    console.log(await page.goto('https://opoznai.bg/all/sort:popular_ever'));
+    await page.waitForRequest()
     await page
 
-    await browser.close();
+    //await browser.close();
 })();
